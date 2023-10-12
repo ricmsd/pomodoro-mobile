169f69d577f3df6679ec0bc766d08bb1e187166b
ricmsd <ricmsd@gmail.com>
2023-10-12 21:30:55 +0900

    Application name changed.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pomodoro-mobile',
+  appName: 'Pomodoro',
   webDir: 'www',
   server: {
     androidScheme: 'https'
